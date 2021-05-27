a1328b308278d61a1abf0d0f64561390cb288198
Wagner Cupertino Lemos <wagner.lemos@ses.ba>
2021-05-27 17:42:24 -0300

    Rest testado

---
+++ src/main/java/com/lemos/cursomc/resources/CategoryResource.java
@@ -0,0 +1,16 @@
+package com.lemos.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoryResource {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {
+		return "Funfou";
+	}
+
+}
