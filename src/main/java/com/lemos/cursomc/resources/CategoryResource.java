24edf8eefad45da75d376f211c64be8270b341cf
Wagner Cupertino Lemos <wagner.lemos@ses.ba>
2021-05-28 09:38:08 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/lemos/cursomc/resources/CategoryResource.java
+++ src/main/java/com/lemos/cursomc/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.lemos.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lemos.cursomc.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoryResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Funfou";
+	public List<Category> listar() {
+		
+		Category cat1 = new Category(1, "Informática");
+		Category cat2 = new Category(2, "Escritório");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 
 }
