3d95de49a36b302a60dd97b9a3fb69ea07f738b3
Wagner Cupertino Lemos <wagner.lemos@ses.ba>
2021-05-28 14:13:31 -0300

    Criando operacao de instanciacao

--- src/main/java/com/lemos/cursomc/CursomcApplication.java
+++ src/main/java/com/lemos/cursomc/CursomcApplication.java
@@ -1,13 +1,35 @@
 package com.lemos.cursomc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.lemos.cursomc.domain.Category;
+import com.lemos.cursomc.repository.CategoryRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
 	}
 
 }
