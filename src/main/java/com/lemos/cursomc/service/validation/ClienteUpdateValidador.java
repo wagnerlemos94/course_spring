6e9fb1e506ece075566e2d1543952c66e13fae74
OEM Configuration (temporary user) <wagner.lemos94@hotmail.com>
2021-06-07 23:11:34 -0300

    Validete update ClienteDTO com bug

---
+++ src/main/java/com/lemos/cursomc/service/validation/ClienteUpdateValidador.java
@@ -0,0 +1,56 @@
+package com.lemos.cursomc.service.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.lemos.cursomc.domain.Cliente;
+import com.lemos.cursomc.domain.dto.ClienteDTO;
+import com.lemos.cursomc.repository.ClienteRepository;
+import com.lemos.cursomc.resources.excption.FieldMessage;
+
+public class ClienteUpdateValidador implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+
+		@SuppressWarnings("unchecked")
+		Map<String,String> map = (Map<String,String>)request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+//		Integer uriId = 2;
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if(aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessege()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
